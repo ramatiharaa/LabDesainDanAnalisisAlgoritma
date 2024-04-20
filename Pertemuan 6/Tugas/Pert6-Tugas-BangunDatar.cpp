2044d30fe3be4e074c7ad0459042033158056718
ramatiharaa <ramatihara@gmail.com>
2024-04-20 12:12:52 +0700

    update

--- Pertemuan 6/Tugas/Pert6-Tugas-BangunDatar.cpp
+++ Pertemuan 6/Tugas/Pert6-Tugas-BangunDatar.cpp
@@ -26,22 +26,34 @@ class Segitiga: public BangunDatar {
     } 
 };
 
+class Persegi: public BangunDatar { 
+    public: 
+    int getValue() { 
+        return (lebar * tinggi); 
+    } 
+};
+
 int main () { 
     PersegiPanjang persegiPanjang; 
     Segitiga segitiga; 
+    Persegi persegi;
     
-    int PPPanjang, PPLebar, SGAlas, SGTinggi;
+    int PPPanjang, PPLebar, SGAlas, SGTinggi, PRPanjang, PRLebar;
     
     cout << "Masukkan panjang persegi panjang: "; cin >> PPPanjang;
     cout << "Masukkan lebar persegi panjang: "; cin >> PPLebar;
     cout << "Masukkan alas segitiga: "; cin >> SGAlas;
     cout << "Masukkan tinggi segitiga: "; cin >> SGTinggi;
+    cout << "Masukkan panjang persegi: "; cin >> PRPanjang;
+    cout << "Masukkan lebar persegi: "; cin >> PRLebar;
     
     persegiPanjang.setValue(PPPanjang, PPLebar); 
     segitiga.setValue(SGAlas, SGTinggi);
+    persegi.setValue(PRPanjang, PRLebar);
     
     cout << "\nLuas Bangun Datar: ";
     cout << "\nLuas Persegi Panjang: " << persegiPanjang.getValue();
     cout << "\nLuas Segitiga: " << segitiga.getValue();
+    cout << "\nLuas Persegi: " << persegi.getValue();
     return 0; 
 }
